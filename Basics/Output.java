e0a6b3ccb1a7285072dec80c6163333ae9a383fa
Yash Izate <yashizate.softech@gmail.com>
2025-01-09 02:29:19 +0530

    update

---
+++ Basics/Output.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class Output {
+  public static void main(String[] args) {
+    
+    // simple print as it is
+    System.out.print("Hello World");
+    System.out.println();
+
+    // \n - print new line after it
+    System.out.print("Hello India \nNamaste Bharat");
+    System.out.println();
+
+    // println - new line after printing when it is used
+    System.out.println("Hello India");
+    System.out.println();
+
+    // printf - it allows us to format strings
+    System.out.printf("Hi, My name is %s. I am %d years old.", "Yash", 21);
+    System.out.println();
+
+  }
+  
+}
