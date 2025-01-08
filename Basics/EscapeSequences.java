3f1705e2a20eaa935beebee299ab635a8d139cbe
Yash Izate <yashizate.softech@gmail.com>
2025-01-09 01:17:25 +0530

    update

--- Basics/EscapeSequences.java
+++ Basics/EscapeSequences.java
@@ -4,22 +4,22 @@ public class EscapeSequences {
   public static void main(String[] args) {
 
     // \n next line after this
-    System.out.println("My name is Yash. \nMy age is 21.");
+    System.out.println("1.*\n  * *\n  * * *\n  * * * *\n  * * * * *");
 
     // \t inserts a tab in the text at this point
-    System.out.println("I live in\tIndia");
+    System.out.println("2. I live in\tIndia");
 
     // \b inserts a backspace in the text at this point (n is removed)
-    System.out.println("Yeshwantrao Chavan\b College of Engineering");
+    System.out.println("3. Yeshwantrao Chavan\b College of Engineering");
 
     // \\ - inserts a backlash
-    System.out.println("Where is address? 150\\2 Bus Stand");
+    System.out.println("4. Where is address? 150\\2 Bus Stand");
 
     // \" - inserts a double quote character
-    System.out.println("What is going in \"Class\" ");
+    System.out.println("5. What is going in \"Class\" ");
 
     // \' - inserts a single quote character
-    System.out.println("Hello \'Kitty\' ");
+    System.out.println("6. Hello \'Kitty\' ");
 
   }
 
