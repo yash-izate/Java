be5079f1f87e4fac1dd96df61ba3274a0431d4bc
Yash Izate <yashizate.softech@gmail.com>
2025-01-09 03:18:11 +0530

    Operators started

---
+++ Operators/Assignment.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class Assignment {
+  public static void main(String[] args) {
+    
+    String myName = "Yash";
+    String yourName = myName;
+
+    System.out.println(yourName);
+
+    int myInt = 9;
+    int yourInt = myInt;
+
+    System.out.println(yourInt);
+
+  }
+  
+}
