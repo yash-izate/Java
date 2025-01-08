24e11e2fb23e75d0f2d3368ef7cf585c9b152ada
Yash Izate <yashizate.softech@gmail.com>
2025-01-09 01:55:46 +0530

    User input then sum added

---
+++ Operators/UserSum.java
@@ -0,0 +1,25 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class UserSum {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Welcome to Calculator!");
+    
+    System.out.print("Enter first number: ");
+    int firstNum = sc.nextInt();
+
+    System.out.print("Enter second number: ");
+    int secondNum = sc.nextInt();
+
+    int sum = firstNum + secondNum;
+
+    System.out.println("Your sum is " + sum + "\nThank You");
+    sc.close();
+
+    
+  }
+  
+}
