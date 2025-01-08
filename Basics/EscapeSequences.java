d5561234dee8cb90a3de188834acc6486f8abe29
Yash Izate <yashizate.softech@gmail.com>
2025-01-09 01:14:00 +0530

    added escape sequenses

---
+++ Basics/EscapeSequences.java
@@ -0,0 +1,26 @@
+package Basics;
+
+public class EscapeSequences {
+  public static void main(String[] args) {
+
+    // \n next line after this
+    System.out.println("My name is Yash. \nMy age is 21.");
+
+    // \t inserts a tab in the text at this point
+    System.out.println("I live in\tIndia");
+
+    // \b inserts a backspace in the text at this point (n is removed)
+    System.out.println("Yeshwantrao Chavan\b College of Engineering");
+
+    // \\ - inserts a backlash
+    System.out.println("Where is address? 150\\2 Bus Stand");
+
+    // \" - inserts a double quote character
+    System.out.println("What is going in \"Class\" ");
+
+    // \' - inserts a single quote character
+    System.out.println("Hello \'Kitty\' ");
+
+  }
+
+}
