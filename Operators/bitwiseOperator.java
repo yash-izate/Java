93effb9c29be8979a39d61ea0179a7a1769d0fa5
Yash Izate <yashizate.softech@gmail.com>
2025-01-09 21:20:41 +0530

    bitwise operator

--- Operators/bitwiseOperator.java
+++ Operators/bitwiseOperator.java
@@ -1,5 +1,40 @@
 package Operators;
 
-public Class BitwiseOperator {
-    
+import java.util.Scanner;
+
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // AND & , OR | , XOR ^ , NOT ~ , Left Shift << , Right Shift >>
+
+        System.out.println("Welcome to Bitwise Operator!");
+
+        System.out.print("Enter first number: ");
+        int x = sc.nextInt();
+        System.out.print("Enter first number: ");
+        int y = sc.nextInt();
+
+        int and = x & y;
+        System.out.println("AND: " + and);
+
+        int or = x | y;
+        System.out.println("OR: " + or);
+
+        int xor = x ^ y;
+        System.out.println("XOR: " + xor);
+
+        int not = ~x;
+        System.out.println("NOT: " + not);
+
+        int leftShift = x << y;
+        System.out.println("Left Shift: " + leftShift);
+
+        int rightShift = x >> y;
+        System.out.println("Right Shift: " + rightShift);
+
+        sc.close();
+
+    }
+
 }
