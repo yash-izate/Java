984a20f0ca8507db3d6b2b233277e8849020de81
Yash Izate <yashizate.softech@gmail.com>
2025-01-09 20:43:44 +0530

    update

--- Operators/bitwiseOperator.java
+++ Operators/bitwiseOperator.java
@@ -1,19 +1,5 @@
 package Operators;
 
-import java.util.Scanner;
-
-public class BitwiseOperator {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the number : ");
-        int num = sc.nextInt();
-
-        if ((num & 1) == 0) {
-            System.out.print("Number is Even");
-        } else {
-            System.out.println("Number is Odd");
-        }
-        sc.close();
-    }
+public Class BitwiseOperator {
+    
 }
--- Programs/OddEven.java
+++ Programs/OddEven.java
@@ -22,3 +22,23 @@ public class OddEven {
   }
 
 }
+
+
+
+// import java.util.Scanner;
+
+// public class BitwiseOperator {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter the number : ");
+//         int num = sc.nextInt();
+
+//         if ((num & 1) == 0) {
+//             System.out.print("Number is Even");
+//         } else {
+//             System.out.println("Number is Odd");
+//         }
+//         sc.close();
+//     }
+// }
