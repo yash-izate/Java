f71fc987f6f626089129664fe1d51edbff85d91f
Yash Izate <yashizate.softech@gmail.com>
2025-01-09 12:14:48 +0530

    update basics

---
+++ Basics/If.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class If {
+  public static void main(String[] args) {
+
+    // isMale = false;
+    boolean isMale = true;
+    String name = "Yash";
+
+    System.out.println("Before If");
+    if (isMale) {
+      System.out.println("Mr " + name);
+
+    }
+
+    System.out.println("After If");
+  }
+}
