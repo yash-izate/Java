5b55cfce80b43cc5b2013ead1a7e091456b9271c
Yash Izate <yashizate.softech@gmail.com>
2025-01-09 21:46:13 +0530

    comments in java

---
+++ Basics/Comments.java
@@ -0,0 +1,24 @@
+package Basics;
+
+
+/** Java Docs - 
+ * This is my main 
+ * method
+ */
+public class Comments {
+  public static void main(String[] args) {
+    
+    //  single line comment in java
+
+    /* Multi 
+     * line 
+     * comment
+     * in
+     * java
+     */
+
+    
+     System.out.println("Comments in java \n single line \n multi line \n Java docs");
+  }
+  
+}
