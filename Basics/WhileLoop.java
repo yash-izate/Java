25b4c7f4fb2944d7c98f56fd122db0bbc51d5196
Yash Izate <yashizate.softech@gmail.com>
2025-01-10 10:41:31 +0530

    while loop added

---
+++ Basics/WhileLoop.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class WhileLoop {
+  public static void main(String[] args) {
+
+    // initillization
+    int i = 1;
+
+    // condition
+    while (i <= 10) {
+
+      System.out.println(i);
+
+      // updation
+      i++;
+
+    }
+  }
+}
