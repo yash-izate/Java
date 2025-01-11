1b5b8f5445d54d0b621a4c99ebbafc306228a56f
Yash Izate <yashizate.softech@gmail.com>
2025-01-11 16:45:33 +0530

    modified array reversal

--- Arrays/ArrayReverse.java
+++ Arrays/ArrayReverse.java
@@ -10,35 +10,35 @@ public class ArrayReverse {
         int size = sc.nextInt();
         int[] numArr = new int[size];
         System.out.println("Enter the elements of array below : ");
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             numArr[i] = sc.nextInt();
         }
-        System.out.print("Original array : " );
-        for(int i = 0; i < numArr.length; i++) {
+        System.out.print("Original array : ");
+        for (int i = 0; i < numArr.length; i++) {
             System.out.print(numArr[i] + "  ");
         }
-        
+
         System.out.println();
-        
+
         reverse(numArr);
-        
+
         System.out.print("Reversed Array : ");
-        for(int i = 0; i < numArr.length; i++) {
+        for (int i = 0; i < numArr.length; i++) {
             System.out.print(numArr[i] + "  ");
         }
-        sc.close();        
+        sc.close();
 
     }
 
     public static void reverse(int[] numArr) {
         int i = 0;
 
-        while (i<numArr.length/2) { 
+        while (i < numArr.length / 2) {
             int swap = numArr[i];
             numArr[i] = numArr[numArr.length - 1 - i];
             numArr[numArr.length - 1 - i] = swap;
             i++;
         }
-    } 
+    }
 
 }
