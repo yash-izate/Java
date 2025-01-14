c724b11b233da8acc525c8ff6005da3dc67a5525
Yash Izate <yashizate.softech@gmail.com>
2025-01-14 15:57:13 +0530

    for each loop

---
+++ Basics/ForEachLoop.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class ForEachLoop {
+  public static void main(String[] args) {
+
+    String[] names = { "Yash", "Manoj", "Sangita", "Saloni" };
+
+    for (String name : names) {
+      System.out.println(name);
+    }
+  }
+}
