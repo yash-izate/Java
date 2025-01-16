818f3944d8eca3b16a6da07a336ee5c2cb9dde21
Yash Izate <yashizate.softech@gmail.com>
2025-01-17 01:18:02 +0530

    modify

--- OOPs/Polymorphism/Overloading.java
+++ OOPs/Polymorphism/Overloading.java
@@ -1,19 +1,26 @@
 package OOPs.Polymorphism;
 
 public class Overloading {
-  
+
   public int sum(int x, int y) {
     return x + y;
   }
 
   public int sum(int x, int y, int z) {
-    return x+y+z;
+    return x + y + z;
+  }
+
+  public String sum(String a, String b) {
+    return "Welcome! " + a + " " + b;
   }
 
   public static void main(String[] args) {
     Overloading overload = new Overloading();
+    
+    System.out.println(overload.sum("Yash","Izate"));
 
     System.out.println(overload.sum(11, 9));
     System.out.println(overload.sum(2, 3, 5));
+
   }
 }
