6118965c7cd787d57110bd9313d8fdf3d1a558e4
Yash Izate <yashizate.softech@gmail.com>
2025-03-15 19:54:50 +0530

    update

---
+++ Loops/square.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class square {
+  public static void main(String[] args) {
+
+    int n = 4;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
