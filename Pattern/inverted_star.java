9290591c58f2e31131be1034afb64c0f1c0f0652
Yash Izate <yashizate.softech@gmail.com>
2025-03-16 01:12:45 +0530

    update

--- Pattern/inverted_star.java
+++ Pattern/inverted_star.java
@@ -6,7 +6,7 @@ public class inverted_star {
     int n = 5;
 
     for (int i = 0; i <= n; i++) {
-      for (int j = 0; j <= n - i; j++) {
+      for (int j = 0; j <= n - i -1; j++) {
 
         System.out.print("* ");
 
