f6734d27a8be63d1910a987188321e37be5befcf
Yash Izate <yashizate.softech@gmail.com>
2025-03-16 01:41:48 +0530

    add folder

---
+++ Functions/HelloWorld.java
@@ -0,0 +1,15 @@
+package Functions;
+
+public class HelloWorld {
+
+  // function definition
+  public static void printHello() {
+    System.out.println("Hello World!");
+  }
+
+  public static void main(String[] args) {
+
+    // function call
+    printHello(); 
+  }
+}
