0f24d3841d6412695b69e7f51bd323773815bbd5
Yash Izate <yashizate.softech@gmail.com>
2025-03-16 01:20:26 +0530

    update

--- Pattern/star_pattern.java
+++ Pattern/star_pattern.java
@@ -3,7 +3,7 @@ package Pattern;
 public class star_pattern {
   public static void main(String[] args) {
 
-    int n = 10;
+    int n = 5;
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j <= i; j++) {
