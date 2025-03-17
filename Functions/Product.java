cc5a9746ce70a0b4b2943a78b0e1c14ff7c3084a
Yash Izate <yashizate.softech@gmail.com>
2025-03-17 10:08:00 +0530

    add product

--- Functions/Product.java
+++ Functions/Product.java
@@ -1,7 +1,17 @@
 package Functions;
 
 public class Product {
+
+  public static void product(int a, int b) {
+    int product = a * b;
+    System.out.println("Product of " + a + " and " + b + " is " + product);
+  }
+
   public static void main(String[] args) {
-    
+    int a = 5;
+    int b = 10;
+
+    product(a, b);
+
   }
 }
